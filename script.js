43b48849548cc4225e550de90c3908cdb4d17f9f
TuLLuR <tullur69@mail.ru>
2017-07-30 18:22:20 +0300

    Ok
    Gj

--- script.js
+++ script.js
@@ -2,39 +2,28 @@
 $(".btn").click(function () {
     var textArea = $("#inputVal");
     var str = (textArea.val()).toString();
-    var p = Math.PI;
-    var e = Math.E;
 
     if (this.innerHTML != "=" && this.innerHTML != "√") {
         if (str != "") {
-            if (
-                (str[str.length - 1] == '/' || str[str.length - 1] == '+' || str[str.length - 1] == '-' || str[str.length - 1] == '*' || str[str.length - 1] == "p" || str[str.length - 1] == ".") &&
-                (this.innerHTML == '/' || this.innerHTML == '+' || this.innerHTML == '-' || this.innerHTML == '*' || this.innerHTML == "p" || this.innerHTML == '.')) {} else {
+            if ((isNaN(str[str.length - 1]) && str[str.length - 1] != ")") && (isNaN(this.innerHTML)) && this.innerHTML != "(" && this.innerHTML != ")") {} else if (this.innerHTML == "." && checkNumber(findNum(textArea.val()))) {
+                textArea.val(textArea.val() + ".");
+            } else if (this.innerHTML == "." && checkNumber(findNum(textArea.val())) == false) {} else {
                 textArea.val(textArea.val() + this.innerHTML);
             }
-        } else if (str == "" && (this.innerHTML != '/' && this.innerHTML != '+' && this.innerHTML != '*' && this.innerHTML != '.')) {
+        } else if (str == "" && !(isNaN(this.innerHTML)) || this.innerHTML == "(" || this.innerHTML == "-") {
             textArea.val(textArea.val() + this.innerHTML);
         }
-
-    }
-    //    else if (this.innerHTML == "p") {
-    //        textArea.val(textArea.val() + "p");
-    //    } 
-    //    else if (this.innerHTML == 'e') {
-    //        textArea.val(textArea.val() + "e");
-    //} 
-    else if (this.innerHTML == "√") {
+    } else if (this.innerHTML == "√") {
         textArea.val(Math.sqrt(textArea.val()));
     } else {
         textArea.val(eval(textArea.val()));
     }
-    //Если нажата кнопка "с", то стирает все из текстового поля
+
     if (this.innerHTML == "c") {
+
         textArea.val("");
-        //textArea.val(str.substring(0, str.length - 1));
     }
 });
-
 
 var textarea = document.querySelector('#inputVal');
 var btn = document.querySelectorAll('button');
@@ -58,4 +47,24 @@ function playAudio() {
     myAudio.volume = 0.05;
     myAudio.src = "M1.mp3";
     myAudio.play();
+}
+
+function checkNumber(x) {
+    if (x % 1 == 0) {
+        return true;
+    } else
+        return false;
+}
+
+function findNum(str) {
+    for (var i = str.length - 1; i >= 0; i--) {
+        if (isNaN(str[i])) {
+            if (str[i] == ".") {
+                continue;
+            }
+            return str.substring(i + 1, str.length);
+        }
+
+    }
+    return str;
 }
